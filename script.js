286b50e2275598ec7b02eac2ef9376193411ce57
unknown <daria.lantyugova@mail.ru>
2023-04-25 15:33:48 +0300

    planner (2nd version)

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ const inputElement = document.querySelector('.input-part__input'); //поле в
 
 const outputElement = document.querySelector('.output-part__tasks'); //поле вывода списка задач
 
-
 // функция добавления задач в массив и их вывода в output-part
 let tasks = [];
 
 const showTasks = () => {
 const inputValue = inputElement.value;
 tasks.push(inputValue);
-outputElement.innerHTML += `<p class="output-part__item">${inputValue} <input id="checkbox" type="checkbox"</p>`;
+outputElement.innerHTML += `<p class="output-part__item-filled">${inputValue} <input id="checkbox" type="checkbox"</p>`;
+if(tasks.lenghth !==0) {
+    clearButton.removeAttribute('disabled', true);
+}
 }
 
 
@@ -25,8 +27,9 @@ addButton.addEventListener('click', showTasks );
 //функция удаления задач из массива и очищения output-part__tasks и делаем кнопку Очистить неактивной
 const clearTasks = () => {
     tasks.splice(0);
-    outputElement.innerHTML = `<p class="output-part__item></p>`;
+    outputElement.innerHTML = `<p class="output-part__item-cleared">Нет задач</p>`;
     clearButton.setAttribute('disabled', true);
+
 }
 
 //вешаем обработчик событий на кнопку Очистить
