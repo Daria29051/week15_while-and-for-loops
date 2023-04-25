2f107b45a07567a806d0f17d67080c14bf2209de
unknown <daria.lantyugova@mail.ru>
2023-04-25 15:39:46 +0300

    planner (3rd version)

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const inputElement = document.querySelector('.input-part__input'); //поле в
 
 const outputElement = document.querySelector('.output-part__tasks'); //поле вывода списка задач
 
+const outputElementNoTasks = document.querySelector('.output-part__no-tasks'); //поле вывода "Нет задач"
+
 // функция добавления задач в массив и их вывода в output-part
 let tasks = [];
 
 const showTasks = () => {
 const inputValue = inputElement.value;
 tasks.push(inputValue);
+outputElementNoTasks.innerHTML = '';
 outputElement.innerHTML += `<p class="output-part__item-filled">${inputValue} <input id="checkbox" type="checkbox"</p>`;
 if(tasks.lenghth !==0) {
     clearButton.removeAttribute('disabled', true);
@@ -27,7 +30,8 @@ addButton.addEventListener('click', showTasks );
 //функция удаления задач из массива и очищения output-part__tasks и делаем кнопку Очистить неактивной
 const clearTasks = () => {
     tasks.splice(0);
-    outputElement.innerHTML = `<p class="output-part__item-cleared">Нет задач</p>`;
+    outputElementNoTasks.innerHTML = `<p class="output-part__item-cleared">Нет задач</p>`;
+    outputElement.innerHTML = '';
     clearButton.setAttribute('disabled', true);
 
 }
